bc47d1ce28261206c674b738ea1f81803cbb4db8
yasrre <ahannach.yassine@etu.uae.ac.ma>
2025-10-19 17:48:34 -0400

    Update Dockerfile CMD to main.py

--- api/main.py
+++ api/main.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/') 
 def hello():
     return "Welcome to the Arithmetic API! Use /add, /subtract, /multiply, /divide endpoints."
